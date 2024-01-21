5c1c24ae9e784abd56d03a0489cb9d442b050684
OMAKHMUDOFF <omaxmudovv@gmail.com>
2024-01-21 21:12:23 +0500

    process.env not fixed

--- index.js
+++ index.js
@@ -16,23 +16,51 @@ const saveToken = async (token) => {
   }
 };
 
+const saveCity = async (city) => {
+  if (!city.length) {
+    printErr("City doesn't exist");
+    return;
+  }
+  try {
+    await saveKeyVal(TOKEN_DICTIONARY.city, city);
+    printSucc("City was saved");
+  } catch (error) {
+    printErr(error.message);
+  }
+};
+
+const getForcast = () => {
+  try {
+    const resp = getWeather(process.env.CITY ?? "Uzbekistan");
+    console.log(resp);
+  } catch (error) {
+    if (error?.resp.status == 404) {
+      printErr("City not found");
+    } else if (error?.resp?.status == 401) {
+      printErr("Invalid Token");
+    } else {
+      printErr(error.message);
+    }
+  }
+};
+
 const startCLI = () => {
   const args = getArgs(process.argv);
   console.log(process.env);
   if (args.h) {
-    printHelp();
+    return printHelp();
     // help
   }
   if (args.s) {
+    return saveCity(args.s);
     // save city
   }
   if (args.t) {
     return saveToken(args.t);
     // save token
   }
-
+  getForcast();
   // result
-  getWeather(process.env.CITY ?? "uzbekistan");
 };
 
 startCLI();
--- services/api.service.js
+++ services/api.service.js
@@ -21,6 +21,7 @@ const getWeather = async (city) => {
     }
   );
   console.log(data);
+  return data;
 
   // const url = new URL("https://api.openweathermap.org/data/2.5/weather");
   // url.searchParams.append("q", city);
